24784b0f200a0a8184c63990c09986cb72cced9d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-21 13:38:30 +0800

    impl registration only check, delete impls for withdraw and unregister

--- contract/factory/src/storage_management.rs
+++ contract/factory/src/storage_management.rs
@@ -1,7 +1,9 @@
 use near_contract_standards::storage_management::{
     StorageBalance, StorageBalanceBounds, StorageManagement,
 };
-use near_sdk::{assert_one_yocto, env, json_types::U128, log, near_bindgen, AccountId, Promise};
+use near_sdk::{
+    assert_one_yocto, env, json_types::U128, log, near_bindgen, require, AccountId, Promise,
+};
 
 use crate::factory_contract::{Factory, FactoryExt};
 
@@ -37,66 +39,60 @@ impl StorageManagement for Factory {
         account_id: Option<AccountId>,
         registration_only: Option<bool>,
     ) -> StorageBalance {
-        // TODO: deal with registration_only
-
         let amount = env::attached_deposit();
         let account_id = account_id.unwrap_or_else(env::predecessor_account_id);
-        if self.storage_balances.contains_key(&account_id) {
-            log!("The account is already registered, refunding the deposit");
-            if amount > 0 {
-                Promise::new(env::predecessor_account_id()).transfer(amount);
+        let registered = self.storage_balances.contains_key(&account_id);
+        let registration_only = registration_only.unwrap_or_else(|| false);
+
+        if registered {
+            let storage_balance = self.storage_balances.get_mut(&account_id).unwrap();
+
+            if registration_only {
+                log!("The account is already registered, refunding the deposit");
+                if amount > 0 {
+                    Promise::new(env::predecessor_account_id()).transfer(amount);
+                }
+            } else {
+                storage_balance.total = (storage_balance.total.0 + amount).into();
+                storage_balance.available = (storage_balance.available.0 + amount).into();
             }
         } else {
-            let min_balance = self.storage_balance_bounds().min.0;
-            if amount < min_balance {
-                env::panic_str("The attached deposit is less than the minimum storage balance");
-            }
             self.internal_register_account(account_id.clone());
-            let refund = amount - min_balance;
-            if refund > 0 {
-                Promise::new(env::predecessor_account_id()).transfer(refund);
+            let min_balance = self.storage_balance_bounds().min.0;
+
+            require!(
+                amount > min_balance,
+                "The attached deposit is less than the minimum storage balance"
+            );
+
+            let storage_balance = self.storage_balances.get_mut(&account_id).unwrap();
+
+            if registration_only {
+                storage_balance.total = min_balance.into();
+                let refund = amount - min_balance;
+                if refund > 0 {
+                    Promise::new(env::predecessor_account_id()).transfer(refund);
+                }
+            } else {
+                let available = amount - min_balance;
+                storage_balance.total = amount.into();
+                storage_balance.available = available.into();
             }
         }
+
         self.internal_storage_balance_of(&account_id).unwrap()
     }
 
     #[payable]
     fn storage_withdraw(&mut self, amount: Option<U128>) -> StorageBalance {
         assert_one_yocto();
-        let predecessor_account_id = env::predecessor_account_id();
-        if let Some(storage_balance) = self.internal_storage_balance_of(&predecessor_account_id) {
-            match amount {
-                Some(amount) if amount.0 > 0 => {
-                    env::panic_str("The amount is greater than the available storage balance");
-                }
-                _ => storage_balance,
-            }
-        } else {
-            env::panic_str(
-                format!("The account {} is not registered", &predecessor_account_id).as_str(),
-            );
-        }
+        todo!()
     }
 
     #[payable]
     fn storage_unregister(&mut self, force: Option<bool>) -> bool {
         assert_one_yocto();
-        let account_id = env::predecessor_account_id();
-        let force = force.unwrap_or(false);
-        if let Some(balance) = self.storage_balances.get(&account_id) {
-            if balance.total.0 == 0 || force {
-                self.storage_balances.remove(&account_id);
-                Promise::new(account_id.clone()).transfer(self.storage_balance_bounds().min.0 + 1);
-                true
-            } else {
-                env::panic_str(
-                    "Can't unregister the account with the positive balance without force",
-                )
-            }
-        } else {
-            log!("The account {} is not registered", &account_id);
-            false
-        }
+        todo!()
     }
 
     fn storage_balance_bounds(&self) -> StorageBalanceBounds {
