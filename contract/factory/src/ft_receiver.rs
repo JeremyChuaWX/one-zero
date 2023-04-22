74944869295c0afebbf28edc663f94ff9a76c8a3
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-22 15:39:57 +0800

    scope out ft receiver methods

--- contract/factory/src/ft_receiver.rs
+++ contract/factory/src/ft_receiver.rs
@@ -1,16 +1,34 @@
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
-use near_sdk::near_bindgen;
+use near_sdk::{json_types::U128, near_bindgen, AccountId, PromiseOrValue};
 
 use crate::factory_contract::{Factory, FactoryExt};
+
+#[near_bindgen]
+impl Factory {
+    #[private]
+    pub fn ft_resolve_transfer(
+        &mut self,
+        sender_id: AccountId,
+        receiver_id: AccountId,
+        amount: U128,
+    ) {
+        todo!()
+    }
+}
 
 #[near_bindgen]
 impl FungibleTokenReceiver for Factory {
     fn ft_on_transfer(
         &mut self,
-        sender_id: near_sdk::AccountId,
-        amount: near_sdk::json_types::U128,
+        sender_id: AccountId,
+        amount: U128,
         msg: String,
-    ) -> near_sdk::PromiseOrValue<near_sdk::json_types::U128> {
+    ) -> PromiseOrValue<U128> {
+        /*
+        - msg contains: token id, market id, long or short token
+        - call FT contract to burn the attached FT
+        - transfer associated amount of NEAR
+        */
         todo!()
     }
 }
