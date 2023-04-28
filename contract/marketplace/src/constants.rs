13e482fd73a6de826aa444d83686d7d8e3972908
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-28 10:53:31 +0800

    update gas constants

--- contract/marketplace/src/constants.rs
+++ contract/marketplace/src/constants.rs
@@ -6,7 +6,6 @@ pub const ZERO: u128 = 0;
 pub mod gas {
     use near_sdk::Gas;
 
-    pub const DEPLOY_TOKEN: Gas = Gas(50_000_000_000_000);
-    pub const INIT_TOKEN: Gas = Gas(10_000_000_000_000);
-    pub const CREATE_MARKET: Gas = Gas(10_000_000_000_000);
+    pub const INIT_TOKEN: Gas = Gas(5_000_000_000_000);
+    pub const CREATE_MARKET: Gas = Gas(5_000_000_000_000);
 }
