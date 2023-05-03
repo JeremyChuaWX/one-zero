9e0eb5511f36433b3ba9c809f095367ef46abf89
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-03 20:06:10 +0800

    use jest

--- contract/integration-tests/src/helpers.ts
+++ contract/integration-tests/src/helpers.ts
@@ -1,4 +1,6 @@
-import { NearAccount } from "near-workspaces";
+import { NearAccount, parseNEAR, tGas } from "near-workspaces";
+
+const MAX_GAS = tGas(300);
 
 type Market = {
     id: number;
@@ -13,7 +15,14 @@ type Market = {
 type Offer = {
     id: number;
     market_id: number;
+    account_id: string;
+    is_long: boolean;
+    amount: string;
 };
+
+function numberToNEAR(n: number): string {
+    return parseNEAR(`${n} N`).toString();
+}
 
 async function listMarkets(marketplace: NearAccount): Promise<Market[]> {
     return await marketplace.view("list_markets");
@@ -23,4 +32,47 @@ async function listOffers(marketplace: NearAccount): Promise<Offer[]> {
     return await marketplace.view("list_offers");
 }
 
-export { Market, Offer, listMarkets, listOffers };
+async function createMarket(
+    marketplace: NearAccount,
+    marketOwner: NearAccount,
+    description: string,
+) {
+    await marketOwner.call(
+        marketplace,
+        "create_market",
+        { description },
+        { attachedDeposit: numberToNEAR(5), gas: MAX_GAS },
+    );
+}
+
+async function createOffer(
+    marketplace: NearAccount,
+    offerMaker: NearAccount,
+    market_id: number,
+    isLong: boolean,
+    amount: number,
+) {
+    await offerMaker.call(
+        marketplace,
+        "create_offer",
+        {
+            market_id,
+            is_long: isLong,
+            amount: numberToNEAR(amount),
+        },
+        {
+            attachedDeposit: numberToNEAR(amount + 1),
+            gas: MAX_GAS,
+        },
+    );
+}
+
+export {
+    Market,
+    Offer,
+    numberToNEAR,
+    listMarkets,
+    listOffers,
+    createMarket,
+    createOffer,
+};
--- contract/integration-tests/src/main.test.ts
+++ contract/integration-tests/src/main.test.ts
@@ -1,23 +1,27 @@
+import { Worker, NearAccount } from "near-workspaces";
 import {
-    Worker,
-    NearAccount,
-    captureError,
-    BN,
-    parseNEAR,
-    tGas,
-} from "near-workspaces";
-import anyTest, { TestFn } from "ava";
-import { Market, Offer, listMarkets, listOffers } from "./helpers";
+    Market,
+    Offer,
+    createMarket,
+    createOffer,
+    numberToNEAR,
+} from "./helpers";
+import { listMarkets, listOffers } from "./helpers";
 
-const STORAGE_BYTE_COST = "1.5mN";
 const MARKETPLACE_CONTRACT_PATH = "./wasm/marketplace.wasm";
+const MARKET_DESCRIPTION = "testing";
 
-const test = anyTest as TestFn<{
-    worker: Worker;
+type Context = {
+    worker: Worker | undefined;
     accounts: Record<string, NearAccount>;
-}>;
+};
 
-test.before(async (t) => {
+let context: Context = {
+    worker: undefined,
+    accounts: {},
+};
+
+beforeAll(async () => {
     const worker = await Worker.init();
     const root = worker.rootAccount;
     const marketOwner = await root.createSubAccount("offer");
@@ -25,71 +29,80 @@ test.before(async (t) => {
     const shortAccount = await root.createSubAccount("short");
     const marketplace = await root.devDeploy(MARKETPLACE_CONTRACT_PATH);
     await root.call(marketplace, "new", {});
-    t.context.worker = worker;
-    t.context.accounts = {
-        root,
-        marketplace,
-        marketOwner,
-        longAccount,
-        shortAccount,
+    context = {
+        worker,
+        accounts: {
+            root,
+            marketplace,
+            marketOwner,
+            longAccount,
+            shortAccount,
+        },
     };
 });
 
-test("Create market", async (t) => {
-    const { marketplace, marketOwner } = t.context.accounts;
-    await marketOwner.call(
-        marketplace,
-        "create_market",
-        {
-            description: "testing",
-        },
-        {
-            attachedDeposit: parseNEAR("5 N").toString(),
-            gas: tGas(300),
-        },
-    );
-    const actual: Market[] = [
-        {
-            id: 0,
-            description: "testing",
-            owner_id: marketOwner.toJSON(),
-            is_long: false,
-            is_closed: false,
-            long_token_id: "m0l." + marketplace.toJSON(),
-            short_token_id: "m0s." + marketplace.toJSON(),
-        },
-    ];
-    const markets = await listMarkets(marketplace);
-    t.deepEqual(actual, markets);
+describe("Create market", () => {
+    beforeAll(async () => {
+        const { marketplace, marketOwner } = context.accounts;
+        await createMarket(marketplace, marketOwner, MARKET_DESCRIPTION);
+    });
+
+    test("Market is created properly", async () => {
+        const { marketplace, marketOwner } = context.accounts;
+        const expected: Market[] = [
+            {
+                id: 0,
+                description: MARKET_DESCRIPTION,
+                owner_id: marketOwner.toJSON(),
+                is_long: false,
+                is_closed: false,
+                long_token_id: "m0l." + marketplace.toJSON(),
+                short_token_id: "m0s." + marketplace.toJSON(),
+            },
+        ];
+        const markets = await listMarkets(marketplace);
+        expect(markets).toStrictEqual(expected);
+    });
 });
 
-test("Create offer", async (t) => {
-    const { marketplace, longAccount } = t.context.accounts;
-    await longAccount.call(
-        marketplace,
-        "create_offer",
-        {
-            market_id: 0,
-            is_long: true,
-            amount: parseNEAR("1 N").toString(),
-        },
-        {
-            attachedDeposit: parseNEAR("3 N").toString(),
-            gas: tGas(300),
-        },
-    );
-    const actual: Offer[] = [
-        {
-            id: 0,
-            market_id: 0,
-        },
-    ];
-    const offers = await listOffers(marketplace);
-    t.deepEqual(actual, offers);
+describe("Create offer", () => {
+    let balanceBefore: number = 0;
+    let balanceAfter: number = 0;
+
+    beforeAll(async () => {
+        const { marketplace, longAccount } = context.accounts;
+        balanceBefore = parseFloat(
+            (await longAccount.balance()).available.toString(),
+        );
+        await createOffer(marketplace, longAccount, 0, true, 1);
+        balanceAfter = parseFloat(
+            (await longAccount.balance()).available.toString(),
+        );
+    });
+
+    test("Offer is created properly", async () => {
+        const { marketplace, longAccount } = context.accounts;
+        const expected: Offer[] = [
+            {
+                id: 0,
+                market_id: 0,
+                account_id: longAccount.toJSON(),
+                is_long: true,
+                amount: numberToNEAR(1),
+            },
+        ];
+        const offers = await listOffers(marketplace);
+        expect(offers).toStrictEqual(expected);
+    });
+
+    test("Correct amount of deposit is used", () => {
+        const balanceChange = balanceAfter - balanceBefore;
+        expect(balanceChange).toBe();
+    });
 });
 
-test.after(async (t) => {
-    await t.context.worker.tearDown().catch((err) => {
+afterAll(async () => {
+    await context.worker?.tearDown().catch((err) => {
         console.log("Failed to tear down the worker:", err);
     });
 });
