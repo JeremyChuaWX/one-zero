73ec0c955387a348d230a4bea07d600a851e3338
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-04 15:53:49 +0800

    add market form

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,32 +1,70 @@
 import { type NextPage } from "next";
-import { FormEventHandler, useEffect } from "react";
+import { type FormEventHandler, useEffect, useState } from "react";
 import { useWalletSelector } from "~/contexts/WalletSelectorContext";
-import useMarketStore from "~/stores/marketStore";
+import type { Offer, Market } from "~/types";
+import { getMarkets, addMarket } from "~/utils/market-methods";
+import { addOffer, getOffers } from "~/utils/offer-methods";
 
 const Home: NextPage = () => {
-    const { selector } = useWalletSelector();
-    const { markets, getMarkets } = useMarketStore();
+    const { selector, accountId } = useWalletSelector();
+    const [markets, setMarkets] = useState<Market[]>([]);
+    const [offers, setOffers] = useState<Offer[]>([]);
 
     useEffect(() => {
-        getMarkets(selector).catch(console.error);
+        (async () => {
+            const newMarkets = await getMarkets(selector);
+            setMarkets(newMarkets);
+
+            const newOffers = await getOffers(selector);
+            setOffers(newOffers);
+        })();
     }, []);
 
-    const addMarketOnSumbit: FormEventHandler<HTMLFormElement> = (e) => {
+    const addMarketOnSumbit: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
+        if (!accountId) throw Error("not signed in");
+        await addMarket(selector, accountId, "testing");
+    };
+
+    const addOfferOnSumbit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        if (!accountId) throw Error("not signed in");
+        await addOffer(selector, accountId, 0, true, 1);
     };
 
     return (
         <>
-            <form onSubmit={addMarketOnSumbit}>
-                <label>Market Description</label>
-                <input type="text" />
-                <button type="submit">Add Market</button>
-            </form>
-            {markets.map((market) => {
+            <div className="flex gap-4 items-center">
+                <form
+                    onSubmit={addMarketOnSumbit}
+                    className="flex flex-col gap-2 w-full"
+                >
+                    <label className="font-bold">Market Description</label>
+                    <input type="text" />
+                    <button
+                        type="submit"
+                        className="py-1 px-4 text-gray-200 bg-gray-900 rounded-md"
+                    >
+                        Add Market
+                    </button>
+                </form>
+                <form
+                    onSubmit={addOfferOnSumbit}
+                    className="flex flex-col gap-2 w-full"
+                >
+                    <button
+                        type="submit"
+                        className="py-1 px-4 text-gray-200 bg-gray-900 rounded-md"
+                    >
+                        Add Offer
+                    </button>
+                </form>
+            </div>
+            {markets.map((market) => (
                 <div key={market.id}>
                     {market.id}: {market.description}
-                </div>;
-            })}
+                </div>
+            ))}
         </>
     );
 };
