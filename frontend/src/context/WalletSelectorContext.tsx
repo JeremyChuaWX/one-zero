fc07498bdf858e5139ed0b7db06ba92baa4f4a52
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-04 11:47:55 +0800

    wallet selector context

---
+++ frontend/src/context/WalletSelectorContext.tsx
@@ -0,0 +1,127 @@
+import type { AccountState, WalletSelector } from "@near-wallet-selector/core";
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import type { ReactNode, FC } from "react";
+import {
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+    createContext,
+} from "react";
+import { distinctUntilChanged, map } from "rxjs";
+
+import Loading from "~/components/Loading";
+import { env } from "~/env.mjs";
+
+declare global {
+    interface Window {
+        selector: WalletSelector;
+        modal: WalletSelectorModal;
+    }
+}
+
+interface WalletSelectorContextValue {
+    selector: WalletSelector;
+    modal: WalletSelectorModal;
+    accounts: Array<AccountState>;
+    accountId: string | null;
+}
+
+const WalletSelectorContext = createContext<WalletSelectorContextValue | null>(
+    null
+);
+
+export const WalletSelectorContextProvider: FC<{
+    children: ReactNode;
+}> = ({ children }) => {
+    const [selector, setSelector] = useState<WalletSelector | null>(null);
+    const [modal, setModal] = useState<WalletSelectorModal | null>(null);
+    const [accounts, setAccounts] = useState<Array<AccountState>>([]);
+
+    const init = useCallback(async () => {
+        const _selector = await setupWalletSelector({
+            network: "testnet",
+            debug: true,
+            modules: [setupNearWallet()],
+        });
+        const _modal = setupModal(_selector, {
+            contractId: env.NEXT_PUBLIC_MKTPLC_CONTRACT,
+        });
+        const state = _selector.store.getState();
+        setAccounts(state.accounts);
+
+        window.selector = _selector;
+        window.modal = _modal;
+
+        setSelector(_selector);
+        setModal(_modal);
+    }, []);
+
+    useEffect(() => {
+        init().catch((err) => {
+            console.error(err);
+            alert("Failed to initialise wallet selector");
+        });
+    }, [init]);
+
+    useEffect(() => {
+        if (!selector) {
+            return;
+        }
+
+        const subscription = selector.store.observable
+            .pipe(
+                map((state) => state.accounts),
+                distinctUntilChanged()
+            )
+            .subscribe((nextAccounts) => {
+                console.log("Accounts Update", nextAccounts);
+
+                setAccounts(nextAccounts);
+            });
+
+        const onHideSubscription = modal!.on("onHide", ({ hideReason }) => {
+            console.log(`The reason for hiding the modal ${hideReason}`);
+        });
+
+        return () => {
+            subscription.unsubscribe();
+            onHideSubscription.remove();
+        };
+    }, [selector]);
+
+    if (!selector || !modal) {
+        return <Loading />;
+    }
+
+    const accountId =
+        accounts.find((account) => account.active)?.accountId || null;
+
+    return (
+        <WalletSelectorContext.Provider
+            value={{
+                selector,
+                modal,
+                accounts,
+                accountId,
+            }}
+        >
+            {children}
+        </WalletSelectorContext.Provider>
+    );
+};
+
+export function useWalletSelector() {
+    const context = useContext(WalletSelectorContext);
+
+    if (!context) {
+        throw new Error(
+            "useWalletSelector must be used within a WalletSelectorContextProvider"
+        );
+    }
+
+    return context;
+}
