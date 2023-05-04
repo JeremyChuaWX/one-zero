122f5271971084c5113f1ed7536911cfb1a8314b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-04 10:16:09 +0800

    formatting

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,15 +2,15 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>One Zero</title>
-        <meta name="One Zero" />
-      </Head>
-      <main>hello world!</main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>One Zero</title>
+                <meta name="One Zero" />
+            </Head>
+            <main>hello world!</main>
+        </>
+    );
 };
 
 export default Home;
