6fd64ab4391e5c6e4ec682de142922d3b6f460b2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-16 19:56:56 +0800

    add react query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import { type AppType } from "next/dist/shared/lib/utils";
 import Navbar from "~/components/Navbar";
 import { WalletSelectorContextProvider } from "~/contexts/WalletSelectorContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "~/styles/globals.css";
 import "@near-wallet-selector/modal-ui/styles.css";
+
+const queryClient = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
@@ -13,10 +16,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
                 <title>One Zero</title>
             </Head>
             <WalletSelectorContextProvider>
-                <Navbar />
-                <main className="p-4 w-full h-full">
-                    <Component {...pageProps} />
-                </main>
+                <QueryClientProvider client={queryClient}>
+                    <Navbar />
+                    <main className="p-4 w-full h-full">
+                        <Component {...pageProps} />
+                    </main>
+                </QueryClientProvider>
             </WalletSelectorContextProvider>
         </>
     );
