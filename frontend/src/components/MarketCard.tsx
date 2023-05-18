b4e36d63a5afb3de8a68e346faea65cebf307ca1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-18 13:23:49 +0800

    per market page

--- frontend/src/components/MarketCard.tsx
+++ frontend/src/components/MarketCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Market } from "~/types";
 
 type MarketCardProps = {
@@ -6,8 +7,8 @@ type MarketCardProps = {
 
 const MarketCard = ({ market }: MarketCardProps) => {
     return (
-        <div className="flex justify-between p-4 w-full rounded-md border-2 border-gray-500">
-            <span>{market.description}</span>
+        <div className="flex justify-between p-4 w-full bg-gray-100 rounded-md outline outline-1 outline-gray-500">
+            <Link href={`/markets/${market.id}`}>{market.description}</Link>
             {market.is_closed ? (
                 <span className="font-bold text-red-700">closed</span>
             ) : (
