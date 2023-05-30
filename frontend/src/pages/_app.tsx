4617bcdeaaa500ac13f0322f1a4b284572d7f0b0
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-31 00:29:46 +0800

    format

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient();
