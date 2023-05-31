c42d8e0c044060c1b2d6722b432c0b85d05aca12
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-31 11:44:57 +0800

    add wallet context

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,30 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { WalletSelectorContextProvider } from "@/contexts/WalletSelectorContext";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+
+import "@near-wallet-selector/modal-ui/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient();
 
     return (
-        <ChakraProvider>
-            <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
-            </QueryClientProvider>
-        </ChakraProvider>
+        <>
+            <Head>
+                <title>One Zero</title>
+            </Head>
+            <ChakraProvider>
+                <QueryClientProvider client={queryClient}>
+                    <WalletSelectorContextProvider>
+                        <Navbar />
+                        <Box as="main" paddingX="4">
+                            <Component {...pageProps} />
+                        </Box>
+                    </WalletSelectorContextProvider>
+                </QueryClientProvider>
+            </ChakraProvider>
+        </>
     );
 }
