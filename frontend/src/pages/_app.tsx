1c658231669b8e166b6428b52ff66e6d7705a05c
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-02 08:23:25 +0800

    add viewport meta tag

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -14,6 +14,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>One Zero</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
             </Head>
             <ChakraProvider>
                 <QueryClientProvider client={queryClient}>
