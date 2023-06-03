fe7be2d9997c0beb7195154361605ccd83dc93e2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-03 16:03:43 +0800

    create market modal

---
+++ frontend/src/components/add-market-modal.tsx
@@ -0,0 +1,102 @@
+import { useWalletSelector } from "@/contexts/wallet-selector-context";
+import { useCreateMarket } from "@/utils/contract-methods";
+import {
+    useDisclosure,
+    useToast,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    ButtonGroup,
+    FormControl,
+    FormLabel,
+    Textarea,
+} from "@chakra-ui/react";
+import { useForm, Controller } from "react-hook-form";
+
+type CreateMarketFormInput = {
+    description: string;
+};
+
+const CreateMarketModal = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const toast = useToast();
+
+    const { control, handleSubmit } = useForm<CreateMarketFormInput>();
+
+    const { selector, accountId } = useWalletSelector();
+
+    const { mutate: createMarket } = useCreateMarket();
+
+    const createMarketOnSumbit = handleSubmit((input) => {
+        if (accountId === null) {
+            toast({
+                status: "error",
+                description: "No wallet connected",
+                isClosable: true,
+                position: "bottom-right",
+            });
+            return;
+        }
+
+        createMarket({
+            selector,
+            accountId,
+            description: input.description,
+        });
+    });
+
+    return (
+        <>
+            <Button onClick={onOpen}>Create Market</Button>
+
+            <Modal isOpen={isOpen} onClose={onClose} size="xl" isCentered>
+                <ModalOverlay />
+
+                <ModalContent>
+                    <ModalHeader>Create Market</ModalHeader>
+                    <ModalCloseButton />
+
+                    <ModalBody>
+                        <form
+                            id="create-market-form"
+                            onSubmit={createMarketOnSumbit}
+                        >
+                            <FormControl isRequired>
+                                <FormLabel>Market Description</FormLabel>
+                                <Controller
+                                    name="description"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Textarea
+                                            {...field}
+                                            placeholder="Enter market description here"
+                                        />
+                                    )}
+                                />
+                            </FormControl>
+                        </form>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <ButtonGroup variant="outline">
+                            <Button type="button" onClick={onClose}>
+                                Close
+                            </Button>
+                            <Button form="create-market-form" type="submit">
+                                Create Market
+                            </Button>
+                        </ButtonGroup>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
+
+export default CreateMarketModal;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import MarketCard from "@/components/market-card";
+import { useWalletSelector } from "@/contexts/wallet-selector-context";
+import { useGetMarkets } from "@/utils/contract-methods";
+import { Box, Button, ButtonGroup, Heading, Spinner } from "@chakra-ui/react";
+import CreateMarketModal from "@/components/add-market-modal";
 
 const Home = () => {
-    return <Box>hello</Box>;
+    const { selector } = useWalletSelector();
+
+    const { data: markets, isLoading } = useGetMarkets(selector);
+
+    return (
+        <Box display="flex" flexDir="column" gap="8">
+            <ButtonGroup variant="outline">
+                <CreateMarketModal />
+                <Button>Create Offer</Button>
+            </ButtonGroup>
+            <Box>
+                <Heading size="md" marginBottom="4">
+                    Markets
+                </Heading>
+                {isLoading ? (
+                    <Box display="flex" flexDir="column" alignItems="center">
+                        <Spinner size="lg" />
+                    </Box>
+                ) : (
+                    <Box display="flex" flexDir="column">
+                        {markets?.map((market, idx) => (
+                            <MarketCard market={market} key={idx} />
+                        ))}
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    );
 };
 
 export default Home;
