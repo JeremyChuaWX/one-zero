3faa47e0a8be70cc96c87f3b90836ab716e45266
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-05 16:32:37 +0800

    composition

--- frontend/src/pages/market/[id].tsx
+++ frontend/src/pages/market/[id].tsx
@@ -1,7 +1,19 @@
 import CreateOfferModal from "@/components/create-offer-modal";
 import { useWalletSelector } from "@/contexts/wallet-selector-context";
+import { Market } from "@/types";
 import { useGetMarketById } from "@/utils/contract-methods";
-import { Box, Spinner, Text } from "@chakra-ui/react";
+import {
+    Badge,
+    Box,
+    Card,
+    CardBody,
+    CardHeader,
+    Heading,
+    Spinner,
+    Stack,
+    StackDivider,
+    Text,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 const MarketPage = () => {
@@ -25,16 +37,55 @@ const MarketPage = () => {
 
     return (
         <Box display="flex" flexDir="column" gap="4">
-            <Box>
-                {market !== undefined &&
-                    Object.entries(market).map(([key, value]) => (
-                        <Text key={key}>
-                            {key} : {value}
-                        </Text>
-                    ))}
-            </Box>
-            <CreateOfferModal marketId={market.id} />
+            <MarketInfo market={market} />
+            <Offers marketId={market.id} />
         </Box>
+    );
+};
+
+const Offers = ({ marketId }: { marketId: number }) => {
+    return (
+        <Box display="flex" flexDir="column" gap="4">
+            <Box display="flex" justifyContent="space-between">
+                <Heading size="md">Offers</Heading>
+                <CreateOfferModal marketId={marketId} />
+            </Box>
+        </Box>
+    );
+};
+
+const MarketInfo = ({ market }: { market: Market }) => {
+    return (
+        <Card display="flex" justifyContent="space-between" variant="outline">
+            <CardHeader display="flex" justifyContent="space-between">
+                <Heading size="md" textTransform="uppercase">
+                    Market {market.id}
+                </Heading>
+                <Badge
+                    colorScheme={market.is_closed ? "red" : "green"}
+                    fontSize="lg"
+                    fontWeight="bold"
+                    height="max-content"
+                >
+                    {market.is_closed ? "Closed" : "Open"}
+                </Badge>
+            </CardHeader>
+
+            <CardBody>
+                <Stack divider={<StackDivider />} spacing="4">
+                    {Object.keys(market).filter((value) =>
+                        !["id", "is_closed", "is_long"].includes(value)
+                    ).map((key) => (
+                        <Box>
+                            <Heading size="xs" textTransform="uppercase">
+                                {key}
+                            </Heading>
+                            <Text>{market[key as keyof Market]}</Text>
+                        </Box>
+                    ))}
+                </Stack>
+            </CardBody>
+        </Card>
     );
 };
 
